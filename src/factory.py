a34f2f37ac87b1a05ef5864ce6bfe8e80c3e64b9
ALUNO <aluno@ifsp.edu.br>
2024-03-28 22:06:12 -0300

    criaçao menus.jinja e utils

--- src/factory.py
+++ src/factory.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 from flask import Flask, render_template
-from src.modulos import bootstrap
+from src.modulos import bootstrap, minify
 
 
 def create_app(config_filename: str = 'config.dev.json'):
@@ -26,6 +26,8 @@ def create_app(config_filename: str = 'config.dev.json'):
         sys.exit(1)
 
     bootstrap.init_app(app)
+    if app.config.get ('MINIFY', False):
+        minify.init_app(app)
 
     @app.route('/')
     @app.route('/index')
