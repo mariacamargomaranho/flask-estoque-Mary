6c8c2e2e24c2a4442be82e4c64b2e17778132a60
ALUNO <aluno@ifsp.edu.br>
2024-04-10 21:13:34 -0300

    configuraçao login

--- src/factory.py
+++ src/factory.py
@@ -1,10 +1,12 @@
 import json
 import os
 import sys
+import uuid
 
 from flask import Flask, render_template
-from src.modulos import bootstrap, minify, db
+from src.modulos import bootstrap, minify, db, login
 from src.utils import existe_esquema, seeding
+
 
 
 def create_app(config_filename: str = 'config.dev.json'):
@@ -30,6 +32,21 @@ def create_app(config_filename: str = 'config.dev.json'):
     if app.config.get ('MINIFY', False):
         minify.init_app(app)
     db.init_app(app)
+    login.init_app(app)
+    login.login_view ='auth.login'
+    login.login_message ='Você precisa estar logado para acessar esse endereço'
+    login.login_message_category ='warning'
+
+    @login.user_loader
+    def load_user(user_id):
+        try:
+            auth_id = uuid.UUID(str(user_id))
+        except ValueError:
+            return None
+        sentenca = db.select(User).where(User.id == auth_id)
+        usuario = db.session.execute(sentenca).scalar_one_nome()
+        return usuario
+
 
     with app.app_context():
         if not existe_esquema(app):
@@ -44,5 +61,7 @@ def create_app(config_filename: str = 'config.dev.json'):
     def index():
         return render_template('index.jinja',
                                title='página inicial')
+    from src.blueprints.auth import bp
+    app.register_blueprint(bp)
 
     return app
--- src/models/usuario.py
+++ src/models/usuario.py
@@ -1,13 +1,14 @@
 import uuid
 
+from flask_login import UserMixin
 from sqlalchemy.orm  import mapped_column, Mapped
-from sqlalchemy.types import Uuid, String
+from sqlalchemy.types import Uuid, String, Boolean
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from src.modulos import db
 from src.models.mixin import DataMixin
 
-class User(db.Model, DataMixin):
+class User(db.Model, DataMixin, UserMixin):
     __tablename__ = 'usuarios'
 
     id:Mapped[Uuid] = mapped_column(Uuid(as_uuid=True),primary_key=True, default=uuid.uuid4)
@@ -15,6 +16,11 @@ class User(db.Model, DataMixin):
     email_normalizado: Mapped[str] = mapped_column(String(256), nullable=False, unique=True, index=True)
     hash_password:Mapped[str] = mapped_column(String(256), nullable=False)
 
+    ativo: Mapped[Boolean] = mapped_column(Boolean, nullable= True, default=False)
+
+    @property
+    def is_active(self):
+        return self.ativo
     @property
     def email(self):
         return self.email_normalizado
@@ -28,3 +34,8 @@ class User(db.Model, DataMixin):
         def check_password(self,senha_aberta) -> bool:
             return check_password_hash(self.hash_password, senha_aberta)
 
+    @classmethod
+    def get_by_email(cls, email):
+        sentenca = db.session(User).where(User.email_normalizado == email.lower())
+        usuario = db.session.execute(sentenca).scalar_one_or_none()
+        return usuario
